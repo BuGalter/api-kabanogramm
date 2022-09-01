badaf1aed58a271a748ab3151c77952b0013e358
BuGalter <wardoc78@gmail.com>
2022-09-01 17:49:01 +0300

    feat: Added to main.ts in validationpipe options whitelist and transform fix: Added , to users.module linter error

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.setGlobalPrefix('api/v1');
 
   const options = new DocumentBuilder()
