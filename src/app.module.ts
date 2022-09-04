59dd0ca7b77ba9dcfb66e392812a0cd750925a52
BuGalter <wardoc78@gmail.com>
2022-09-04 18:12:28 +0300

    feat: Added connect to db mysql typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '0.0.0.0',
+      port: 3306,
+      username: 'kaban',
+      password: 'kaban',
+      database: 'kabanogramm',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
