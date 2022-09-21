d96df1cc7869dbf6182b5152351ff5db6c1b9e22
BuGalter <wardoc78@gmail.com>
2022-09-21 21:39:08 +0300

    feat: Added to user service
    feat: Added service for create user
    feat: Added service for update user
    feat: Added service for find user by id
    feat: Added service for find all users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,30 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return {
-      message: `User ${createUserDto.username} added to db`,
-      data: createUserDto,
-    };
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    //Уникальный емайл, по нему проверка есть ли такой пользователь
+    const { username, email, password, age, about, status } = createUserDto;
+
+    const userInDb = await this.userRepository.findOneBy({ email });
+
+    if (userInDb) {
+      const error = { email: 'Email must be unique.' };
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'Input data validation failed',
+          error,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const user = new User();
+    user.username = username;
+    user.email = email;
+    user.password = password;
+    user.age = age;
+    user.about = about;
+    user.status = status;
+
+    return await this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<User> {
+    return await this.userRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return {
-      message: `This action updates a #${id} user`,
-      data: updateUserDto,
-    };
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) {
+      const error = { id: 'Invalid id.' };
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'Input data validation failed',
+          error,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const updatedUser = Object.assign(user, updateUserDto);
+
+    return await this.userRepository.save(updatedUser);
   }
 
   login(loginUserDto: LoginUserDto) {
