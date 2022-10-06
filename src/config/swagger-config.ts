338ad5883cd996011c4787d49a35e4aba95f8787
BuGalter <wardoc78@gmail.com>
2022-10-06 19:48:45 +0300

    feat: Added for swagger config file and interface

---
+++ src/config/swagger-config.ts
@@ -0,0 +1,7 @@
+import { SwaggerConfig } from './interfaces/swagger-config.interface';
+
+export default (): SwaggerConfig => ({
+  title: 'API Kabanogramm',
+  description: 'The API messenger Kabanogramm description',
+  version: '1.0',
+});
