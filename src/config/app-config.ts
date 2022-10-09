086dcf07079a1b140248c4aafbb0263e34ec1131
BuGalter <wardoc78@gmail.com>
2022-10-09 18:42:52 +0300

    delete: Delete from git index config renamed files

--- src/config/app-config.ts
+++
@@ -1,6 +0,0 @@
-import { AppConfig } from './interfaces/app-config.interface';
-
-export default (): AppConfig => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  globalPrefix: 'api/v1',
-});
--- src/config/swagger-config.ts
+++
@@ -1,7 +0,0 @@
-import { SwaggerConfig } from './interfaces/swagger-config.interface';
-
-export default (): SwaggerConfig => ({
-  title: 'API Kabanogramm',
-  description: 'The API messenger Kabanogramm description',
-  version: '1.0',
-});
