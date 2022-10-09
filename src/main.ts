6a9f915e1609f5fd0cb9921beff44d7576949cc5
BuGalter <wardoc78@gmail.com>
2022-10-09 18:39:18 +0300

    change: Rename config files

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import appConfig from './config/app-config';
-import swaggerConfig from './config/swagger-config';
+import appConfig from './config/app.config';
+import swaggerConfig from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
